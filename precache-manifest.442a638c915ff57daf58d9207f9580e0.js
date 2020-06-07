48034de42037fb292e7eb0cd248e1a75924540cd
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-06-07 19:24:11 +0530

    Updates

---
+++ precache-manifest.442a638c915ff57daf58d9207f9580e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fafd28a827b170af19f510a91934210",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e31ee86e68593b1e98df",
+    "url": "/react-tutorial/static/css/main.1dd3daf5.chunk.css"
+  },
+  {
+    "revision": "c3656428e8c29c006982",
+    "url": "/react-tutorial/static/js/2.757013b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.757013b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31ee86e68593b1e98df",
+    "url": "/react-tutorial/static/js/main.6efc43a0.chunk.js"
+  },
+  {
+    "revision": "790f9dca77bcb7c40721",
+    "url": "/react-tutorial/static/js/runtime-main.2b375828.js"
+  }
+]);
